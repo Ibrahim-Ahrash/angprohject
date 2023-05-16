bcc18c85cd6ffd43ff45c1a660f9e58cac438ba2
unknown <ibrahimalahrash9797@gmail.com>
2023-05-16 14:55:08 +0200

    idk

--- src/app/company/company-list/filter-company/filter-company.component.ts
+++ src/app/company/company-list/filter-company/filter-company.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { CompanyserviceService } from 'src/app/@services/companyservice.service';
 import { Observable, forkJoin } from 'rxjs';
+import { FormControl, FormGroup } from '@angular/forms';
 @Component({
   selector: 'app-filter-company',
   templateUrl: './filter-company.component.html',
@@ -18,40 +19,39 @@ export class FilterCompanyComponent implements OnInit {
   product = []
   system = []
 
+  
   sources: Observable<any>[] = [
     this.getcom.getCities(),
     this.getcom.getActivities(),
     this.getcom.getSystemModuls(),
     this.getcom.GetProducts()
   ];
-  // getdata() {
-  //   forkJoin([
-  //     this.getcom.getCities(),
-  //     this.getcom.getActivities,
-  //     this.getcom.getSystemModuls,
-  //     this.getcom.GetProducts
-  //   ]).subscribe({
-  //     next: (res) => {
-  //       this.cities = res[0].JsonArray;
-  //       this.activitie = res[1].JsonArray;
-  //       this.system = res[2].JsonArray;
-  //       this.product = res[3].JsonArray;
-  //       // .filter(v => v.ProductCategoryID_PK == 1)[0].products;
-  //       console.log(res);
 
-  //     }
-  //   })
-  // }
+  filter= new FormGroup({
+    DateFrom: new FormControl(''),
+    DateTo: new FormControl(''),
+    IsRange	: new FormControl(false,),
+    CityID: new FormControl(0),
+    ActivityID: new FormControl(0),
+    SystemModuleID: new FormControl(0),
+    ProductID: new FormControl(0),
+
+  })
+
 
   ngOnInit(): void {
     forkJoin(this.sources).subscribe({
       next: (res) => {
         this.cities = res[0].JsonArray;
+        console.log(this.cities);
         this.activitie = res[1].JsonArray;
         this.system = res[2].JsonArray;
+        console.log(this.activitie);
+        console.log("this.activitie");
+        console.log(this.system);
         this.product = res[3].JsonArray;
         console.log("activites are")
-        console.log(this.activitie)
+        console.log(this.product)
       }
     })
   }
